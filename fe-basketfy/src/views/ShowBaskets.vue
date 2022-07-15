487785f44d3b5228f5969f71e88ab78230940a9d
Thomas J Militello <thomasjmilitello93@gmail.com>
2022-07-15 12:24:49 -0500

    commit message

--- fe-basketfy/src/views/ShowBaskets.vue
+++ fe-basketfy/src/views/ShowBaskets.vue
@@ -101,8 +101,7 @@ export default {
             <div class="title-heading">
               <h4 class="heading fw-medium text-white title-dark mb-3">
                 {{ basket.name }}
-                <br />
-                <span class="col-0.5 btn btn-primary m-1" type="button" v-on:click="toggleEdit()">
+                <!-- <span class="btn btn-primary m-1" type="button" v-on:click="toggleEdit()">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="14"
@@ -115,7 +114,8 @@ export default {
                       d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"
                     />
                   </svg>
-                </span>
+                </span> -->
+                <br />
               </h4>
 
               <p class="username-color">Created by {{ basket.username }}</p>
@@ -139,6 +139,14 @@ export default {
                   v-on:click="toggleEdit(), editBasket()"
                 >
                   Save Changes
+                </span>
+                <span
+                  class="col-2 btn btn-primary m-1 save-changes"
+                  type="button"
+                  v-if="!isEdit"
+                  v-on:click="toggleEdit()"
+                >
+                  Edit Weights
                 </span>
               </div>
             </div>
